a3c0f12ef5cbcefd6225c270c5a1bfe45b441d2c
Samad <samadshaik035@gmail.com>
2022-03-06 15:15:38 +0530

    completed flames.py

--- flames.py
+++ flames.py
@@ -1,45 +1,52 @@
+def flames(name: str, cname: str) -> str:
+    """
+    name: your name
+    cname: crush name
+    
+    return: Friend | Lovers | Affection | Marriage | Enemy | Siblings .
+    """
+    
+    name = name.replace(" ", '').lower()
+    cname = cname.replace(" ", '').lower()
+    
+    # counting repeated letters
+    n = {a: name.count(a) for a in (name)}
+    cn = {a: cname.count(a) for a in (cname)}
+    
+    # removing duplicates
+    tn = {}
+    [tn.update({a: abs(n[a]-cn[a]) if a in cn else n[a]}) for a in name]
+    [tn.update({b: cn[b]}) for b in cname if b not in tn]
 
-from itertools import count
+    counter = sum(tn.values())
+    
+    flames = {
+        "F": "Friends",
+        "L": "Lovers",
+        "A": "Affection",
+        "M": "Marriage",
+        "E": "Enemy",
+        "S": "Siblings"
+    }
+    f = list(flames.keys())
+    si = 0  # start index
+    
+    # striking a letter at every count
+    while f:
+        pop = counter % len(f)-1
+        popped = f.pop(pop)
+        si = pop
+        if f and pop >= 0:
+            f = f[si:] + f[:si]
+    
+    return flames[popped]
 
-# flames = ["Friends", "Lovers", "Affection", "Marriage", "Enemy", "Siblings"]
 
-flames = {
-    "F": "Friends",
-    "L": "Lovers",
-    "A": "Affection",
-    "M": "Marriage",
-    "E": "Enemy",
-    "S": "Siblings"
-}
-
-# name = input("Enter your name: ").replace(" ", "").lower()
-# cname = input("Enter your crush name:").replace(" ", "").lower()
-
-# print(name, cname)
-
-name = "samad"
-cname = "abdus"
-
-n = {a:name.count(a) for a in (name)}
-cn = {a:cname.count(a) for a in (cname)}
-tn = {}  # total_number
-
-[tn.update({a: abs(n[a]-cn[a]) if a in cn else n[a]}) for a in name]
-[tn.update({b: cn[b]}) for b in cname if b not in tn]
-
-count = sum(tn.values())
-f = list(flames.keys())
-# f = "FLAMES"
-start=0
-# j = count
-print(f)
-while len(f)>1:
-    pop = start+(count%len(f)) if count > len(f) else count-1
-    print([pop], end=" ")
-    # pop=start+max(count, len(f))%min(count, len(f))
-    print(len(f), count, [start], [pop], f.pop(pop), f)
-    f = f[start:] + f[:start]
-    # f.pop(pop)
-    # print(f)
-    start+=pop
-print(f)
+if __name__ == "__main__":
+    # name = input("Enter your name: ")
+    # cname = input("Enter your crush name: ")
+    
+    name = "Alexa"
+    cname = "Siri"
+    
+    print(flames(name, cname))
